2f9e305b761dd4d87c463c7e9e97e59ec7b2cf91
Оля Бездудная <lyolya0606@gmail.com>
2023-10-30 23:52:43 +0300

    Add project files.

---
+++ laboratory_2_c_plus_plus_2023_for_vs/work_with_strings.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+void make_map(string str) {
+
+}
---
+++ laboratory_2_c_plus_plus_2023_for_vs/work_with_strings.h
@@ -0,0 +1,8 @@
+#ifndef WORK_WITH_STRINGS
+#define WORK_WITH_STRINGS
+
+#include <string>
+
+void make_map(std::string str);
+
+#endif //WORK_WITH_STRINGS
